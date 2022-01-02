3c23c3a46b15dfb8488189b4e45a3ec779275b63
emre <emrky.2018@icloud.com>
2022-01-02 16:54:10 +0300

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,24 @@
 import React from 'react'
 import { Grid, Image } from 'semantic-ui-react'
+import Image4 from "../asserts/image4.jpg"
+import Image5 from "../asserts/image5.jpg"
+import Image6 from "../asserts/image6.jpg"
 
 const Main = () => (
   <Grid celled>
     <Grid.Row>
-      <Grid.Column width={3}>
-        <Image src='/images/wireframe/image.png' />
+      <Grid.Column width={3} >
+        <Image src={Image6 } style={{height:"75vh"}} />
       </Grid.Column>
-      <Grid.Column width={13}>
-        <Image src='/images/wireframe/centered-paragraph.png' />
+      <Grid.Column width={8}>
+        <Image src={Image5} />
+      </Grid.Column>
+      <Grid.Column width={5}>
+        <Image src={Image4} />
       </Grid.Column>
     </Grid.Row>
 
-    <Grid.Row>
-      <Grid.Column width={3}>
-        <Image src='/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column width={10}>
-        <Image src='/images/wireframe/paragraph.png' />
-      </Grid.Column>
-      <Grid.Column width={3}>
-        <Image src='/images/wireframe/image.png' />
-      </Grid.Column>
-    </Grid.Row>
+   
   </Grid>
 )
 
